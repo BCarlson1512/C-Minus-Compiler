a4e22274813a9786ff7c4622612b3883644d686f
Conor Roberts <Conor@ConorRoberts.com>
2023-02-06 18:06:45 -0500

    updatye

--- C1-Package/SampleParser/Main.java
+++ C1-Package/SampleParser/Main.java
@@ -19,7 +19,7 @@ class Main {
   static public void main(String argv[]) {    
     /* Start the parser */
     try {
-      Lexer scanner = new Lexer(new FileReader(argv[0]));      
+      Lexer scanner = new Lexer(new FileReader(argv[0])); 
       parser p = new parser(new Lexer(new FileReader(argv[0])));
       Absyn result = (Absyn)(p.parse().value);      
       if (SHOW_TREE && result != null) {
