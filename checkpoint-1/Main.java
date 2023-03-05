c521b48f55021f7a6b871cd498a02578f96437c5
Conor Roberts <Conor@ConorRoberts.com>
2023-03-05 17:49:29 -0500

    Support -a flag

--- checkpoint-1/Main.java
+++ checkpoint-1/Main.java
@@ -10,38 +10,53 @@
 
   where gcd.tiny is an test input file for the tiny language.
 */
-   
+
 import java.io.*;
 import absyn.*;
-   
+
 class Main {
   public final static boolean SHOW_TREE = true;
-  static public void main(String argv[]) {    
+
+  static public void main(String argv[]) {
     /* Start the parser */
     try {
-      if (argv[0]=="-a" && argv.length==2){
-        // Get file
-        // Make sure file exists
-        // Second arg is input file
-        // Output file is INPUT_FILE.abs
-        // Make sure errors print to stderr
+      String fileName = argv[0];
 
-        // TODO double check this bad boy
-        System.setOut(new PrintStream(new FileOutputStream(argv[1]+".abs")));
+      // Should we output the abstract syntax tree in a .abs file?
+      Boolean outputAbsyn = false;
+
+      // Do we have -a in our args list?
+      if (argv[0].equals("-a")) {
+        fileName = argv[1];
+        outputAbsyn = true;
+      } else if (argv[1].equals("-a")) {
+        fileName = argv[0];
+        outputAbsyn = true;
       }
-      Lexer scanner = new Lexer(new FileReader(argv[0])); 
-      parser p = new parser(new Lexer(new FileReader(argv[0])));
-      Absyn result = (Absyn)(p.parse().value);      
+
+      if (outputAbsyn) {
+        String[] pathSegments = fileName.split("/");
+        String lastSegment = pathSegments[pathSegments.length - 1];
+
+        // The output file name is the same as the input file name, but with a different file extension.
+        String strippedLastSegment = lastSegment.substring(0, lastSegment.length() - 3);
+
+        System.setOut(new PrintStream(new FileOutputStream(strippedLastSegment + ".abs")));
+      }
+
+      Lexer scanner = new Lexer(new FileReader(fileName));
+      parser p = new parser(new Lexer(new FileReader(fileName)));
+      Absyn result = (Absyn) (p.parse().value);
       if (SHOW_TREE && result != null) {
-         System.out.println("The abstract syntax tree is:");
-         ShowTreeVisitor visitor = new ShowTreeVisitor();
-         result.accept(visitor, 0); 
+        System.out.println("The abstract syntax tree is:");
+        ShowTreeVisitor visitor = new ShowTreeVisitor();
+        result.accept(visitor, 0);
       }
-    } catch (Exception e) {
+    } catch (
+
+    Exception e) {
       /* do cleanup here -- possibly rethrow e */
       e.printStackTrace();
     }
   }
 }
-
-
