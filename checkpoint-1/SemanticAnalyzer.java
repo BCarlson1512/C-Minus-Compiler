2122e41345433feee8e831d2ad576c6ed0d78956
Ben <46061491+BCarlson1512@users.noreply.github.com>
2023-03-07 19:24:45 -0500

    Add boilerplate classes

---
+++ checkpoint-1/SemanticAnalyzer.java
@@ -0,0 +1,8 @@
+public class SemanticAnalyzer {
+    //TODO: implement symbol table class
+    //TODO: Validate Array types (array range is int, index is int)
+    //TODO: Validate assignments
+    //TODO: Validate operations
+    //TODO: Function call validation
+    //TODO: Validate test conditions (must be int)
+}
---
+++ checkpoint-1/SymbolTable.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+public class SymbolTable {
+    //TODO: Implement absyn and symbol packages
+    //TODO: Create hashmaps for each type of symbol
+    //TODO: Testing for simple vars
+    //TODO: Testing for array variables
+    //TODO: Testing for functions/blocks
+    //TODO: Undefined and Redefined errors testing
+}
